d255472c722cc4c0e851016fbb1a615243d1be01
DocSebas7 <81327636+itachi773@users.noreply.github.com>
2026-02-09 21:57:12 -0500

    Update _reset.js

--- plugins/_reset.js
+++ plugins/_reset.js
@@ -1,19 +1,19 @@
-let { Presence } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, args }) => {
-	await conn.updatePresence(m.chat, Presence.composing) 
-	let list = Object.entries(global.DATABASE.data.users)
-	if(!args || !args[0]) {
-		var lim = 10
-	} else {
-		var lim = parseInt(args[0])
-	}
-	list.slice(0, list.length).map(([user, data], i) => (Number(data.warning = lim)))
-		conn.reply(m.chat, `*Advertencia restablecida con éxito ${lim} / user*`, m)
+  let users = global.DATABASE._data.users
+  let lim = args && args[0] ? parseInt(args[0]) : 10
+  if (isNaN(lim)) lim = 10
+
+  for (let user in users) {
+    users[user].warning = lim
+  }
+
+  conn.reply(m.chat, `*Advertencias restablecidas correctamente (${lim} por usuario)*`, m)
 }
-handler.help = ['']
-handler.tags = ['']
+
 handler.command = /^(reset)$/i
 handler.rowner = true
 handler.exp = 0
 handler.fail = null
+
 module.exports = handler
+

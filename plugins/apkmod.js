4cb4fb599c66e09227e25fb71274a029d8bba896
DocSebas7 <81327636+itachi773@users.noreply.github.com>
2026-02-09 22:02:27 -0500

    Update apkmod.js

--- plugins/apkmod.js
+++ plugins/apkmod.js
@@ -1,32 +1,32 @@
-//jangan ubah om plisss
-//om jangan ambil
-//yah om
+let readMore = String.fromCharCode(8206).repeat(4001)
+
 let handler = async m => m.reply(`
 〘 APK LIST 𝗠𝗢𝗗 〙
 ❃❃❃❃❃❃❃❃❃❃❃❃❃❃❃
-      :) *01* 
+      :) *01*
 ❃❃❃❃❃❃❃❃❃❃❃❃❃❃❃
-%readmore
-➨𝗔𝗹𝗶𝗴𝗵𝘁 𝗠𝗼𝘁𝗶𝗼𝗻 𝗺𝗼𝗱
-➢https://apkdone.com/alight-motion/'
-%readmore
-➨𝗞𝗶𝗻𝗲𝗺𝗮𝘀𝘁𝗲𝗿
-➢https://apkdone.com/kinemaster-apk-mods/
-%readmore
-➨𝗜𝗻𝘀𝗵𝗼𝘁
-➢https://apkdone.com/inshot/
-%readmore
-➨𝗙𝗶𝗹𝗺𝗼𝗿𝗮𝗚𝗼
-➢https://apkdone.com/filmorago/
-%readmore
-➨𝗟𝗶𝗴𝗵𝘁𝗿𝗼𝗼𝗺
-➢https://apkdone.com/adobe-lightroom/
-%readmore
-➨𝗣𝗶𝗰𝘀𝗮𝗿𝘁
-➢https://apkdone.com/picsart-apk-mods/
-`.trim()) 
+${readMore}
+➨ 𝗔𝗹𝗶𝗴𝗵𝘁 𝗠𝗼𝘁𝗶𝗼𝗻 mod
+➢ https://apkdone.com/alight-motion/
+${readMore}
+➨ 𝗞𝗶𝗻𝗲𝗺𝗮𝘀𝘁𝗲𝗿
+➢ https://apkdone.com/kinemaster-apk-mods/
+${readMore}
+➨ 𝗜𝗻𝘀𝗵𝗼𝘁
+➢ https://apkdone.com/inshot/
+${readMore}
+➨ 𝗙𝗶𝗹𝗺𝗼𝗿𝗮𝗚𝗼
+➢ https://apkdone.com/filmorago/
+${readMore}
+➨ 𝗟𝗶𝗴𝗵𝘁𝗿𝗼𝗼𝗺
+➢ https://apkdone.com/adobe-lightroom/
+${readMore}
+➨ 𝗣𝗶𝗰𝘀𝗮𝗿𝘁
+➢ https://apkdone.com/picsart-apk-mods/
+`.trim())
+
 handler.help = ['apkmod']
 handler.tags = ['tools']
-handler.command = /^apkmod|aplikasimod$/i
+handler.command = /^(apkmod|aplikasimod)$/i
 
 module.exports = handler

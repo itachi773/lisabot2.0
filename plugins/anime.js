b1b8af000b50547dd4ebbe5956d769226cd4f462
DocSebas7 <81327636+itachi773@users.noreply.github.com>
2026-02-09 21:59:11 -0500

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -1,30 +1,22 @@
-
 let fetch = require('node-fetch')
 
-let handler = async(m, { conn, args, command, usedPrefix }) => {
-  fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/random.txt').then(res => res.text()).then(body => {
-    let randomnime = body.split('\n')
-    let randomnimex = randomnime[Math.floor(Math.random() * randomnime.length)]
-    conn.sendFile(m.chat, randomnimex, '', 'error xd pto!! ', m)
-  }).catch(() => {
-    conn.reply(m.chat, ` !`, m)
-  })
+let handler = async (m, { conn }) => {
+  try {
+    let res = await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/random.txt')
+    let body = await res.text()
+    let list = body.split('\n').filter(v => v)
+    let url = list[Math.floor(Math.random() * list.length)]
+    await conn.sendFile(m.chat, url, 'anime.jpg', '✨ Anime random ✨', m)
+  } catch (e) {
+    conn.reply(m.chat, 'Error al obtener el anime', m)
+  }
 }
 
 handler.help = ['anime']
 handler.tags = ['downloader']
-handler.command = /^(anime)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
+handler.command = /^anime$/i
 handler.exp = 0
 handler.limit = true
 
 module.exports = handler
+

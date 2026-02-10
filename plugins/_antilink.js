b8929d2c6377b6b8ed71bf99adad5339375a8429
DocSebas7 <81327636+itachi773@users.noreply.github.com>
2026-02-09 21:55:46 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,20 +1,25 @@
 let handler = m => m
 
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
-handler.before = function (m, { user }) {
-  if (m.isBaileys && m.fromMe) return true
-  let chat = global.DATABASE.data.chats[m.chat]
-  let isGroupLink = linkRegex.exec(m.text)
+let linkRegex = /chat\.whatsapp\.com\/([0-9A-Za-z]{20,24})/i
 
-  if (chat.antiLink && isGroupLink) {
-    m.reply('Adios zorra:D ojala nunca vuelvas')
-conn.groupRemove(m.chat, [m.sender])
-    if (global.opts['restrict']) {
-       if (!user.isAdmin) return true
-       conn.groupRemove(m.chat, [m.sender])
-    }
-  }
+handler.before = function (m, { conn, user }) {
+  if (!m.isGroup) return true
+  if (m.isBaileys && m.fromMe) return true
+
+  let chat = global.DATABASE.data.chats[m.chat]
+  if (!chat || !chat.antiLink) return true
+
+  let isGroupLink = linkRegex.exec(m.text)
+  if (!isGroupLink) return true
+
+  if (user.isAdmin) return true
+  if (!global.opts['restrict']) return true
+
+  m.reply('Adios zorra:D ojala nunca vuelvas')
+  conn.groupRemove(m.chat, [m.sender])
+
   return true
 }
 
 module.exports = handler
+

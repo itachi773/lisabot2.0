e1fd1e996ef9c230e7a1c68ee5037fcfd4415411
DocSebas7 <81327636+itachi773@users.noreply.github.com>
2026-02-09 22:04:07 -0500

    Update banned.js

--- plugins/banned.js
+++ plugins/banned.js
@@ -1,36 +1,34 @@
 let handler = async (m, { conn, text }) => {
-	if(isNaN(text)) {
-		var number = text.split`@`[1]
-	} else if(!isNaN(text)) {
-		var number = text
-	}
-	
-	if(!text && !m.quoted) return conn.reply(m.chat, `*Give a number or reply chat target.*`, m)
-	if(number.length > 15) return conn.reply(m.chat, `*Format is Invalid.*`, m)
-	
-try {
-	if(text) {
-		var user = number + '@s.whatsapp.net'
-	} else if(m.quoted.sender) {
-		var user = m.quoted.sender
-	} else if(m.mentionedJid) {
-		var user = number + '@s.whatsapp.net'
-	} 
-} catch (e) {
-		} finally {
-			if(global.DATABASE._data.users[user].isBanned == true){
-			  conn.reply(m.chat, `*Target is already isBanned.*`, m)
-			}else {
-        global.DATABASE._data.users[user].isBanned = true
-				global.DATABASE._data.banned += 1
-				var banTotal = global.DATABASE._data.banned
-				conn.reply(m.chat, `*BANNED !*\n\n○ *Total Banned : ${banTotal}*`, m)
-			}
-	}	
+  let user
+
+  if (m.quoted) {
+    user = m.quoted.sender
+  } else if (m.mentionedJid && m.mentionedJid[0]) {
+    user = m.mentionedJid[0]
+  } else if (text) {
+    let number = text.replace(/\D/g, '')
+    if (!number) throw '*Formato inválido.*'
+    if (number.length > 15) throw '*Formato inválido.*'
+    user = number + '@s.whatsapp.net'
+  } else {
+    throw '*Give a number, tag a user, or reply a message.*'
+  }
+
+  let users = global.DATABASE._data.users
+  if (!users[user]) users[user] = {}
+
+  if (users[user].banned) {
+    return conn.reply(m.chat, '*Target is already banned.*', m)
+  }
+
+  users[user].banned = true
+  conn.reply(m.chat, '*BANNED!*', m)
 }
 
-handler.help = ['*62xx*','*@user*','*(reply)*'].map(v => 'ban ' + v)
+handler.help = ['ban @user', 'ban reply', 'ban 62xxx']
 handler.tags = ['owner']
 handler.command = /^ban$/i
 handler.owner = true
+handler.fail = null
+
 module.exports = handler

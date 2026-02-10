c3776e3bca9ab861cf60e853dc964f13078bec5c
DocSebas7 <81327636+itachi773@users.noreply.github.com>
2026-02-09 21:56:15 -0500

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,16 +1,29 @@
 let handler = m => m
 
 let levelling = require('../lib/levelling')
-handler.before = m => {
+
+handler.before = function (m) {
+  if (!m.sender) return true
+
   let user = global.DATABASE.data.users[m.sender]
-	if (!user.autolevelup) return
-	let before = user.level * 1
-	while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-	if (before !== user.level) m.reply(`
+  if (!user) return true
+  if (!user.autolevelup) return true
+
+  let before = user.level * 1
+  while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) {
+    user.level++
+  }
+
+  if (before !== user.level) {
+    m.reply(`
 Felicitaciones, has subido de nivel!
 *${before}* -> *${user.level}*
-Usa *.profile* Para verificar
-	`.trim())
+Usa *.profile* para verificar
+`.trim())
+  }
+
+  return true
 }
 
 module.exports = handler
+
